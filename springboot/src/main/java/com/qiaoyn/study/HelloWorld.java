abdb6975dd08e6bdafb8955b658653161bc3e1d1
qyn123 <1750718285@qq.com>
2020-10-09 13:51:42 +0800

    bbb

---
+++ springboot/src/main/java/com/qiaoyn/study/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.qiaoyn.study;
+
+/**
+ * @author qiaoyn
+ * @description 你好
+ * @date 2020/10/9
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
