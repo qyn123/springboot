6f5b706b0862bf7b1a8591c8e28f30efbc567254
qyn123 <1750718285@qq.com>
2020-10-09 14:33:18 +0800

    xxx提交xxx

---
+++ springboot/src/main/java/com/qiaoyn/study/Test3.java
@@ -0,0 +1,13 @@
+package com.qiaoyn.study;
+
+/**
+ * @author qiaoyn
+ * @description 用于提交远程到github上
+ * @date 2020/10/9
+ */
+public class Test3 {
+    public static void main(String[] args) {
+        System.out.println("大家好，这是我第一次提交代码");
+
+    }
+}
---
+++ springboot/src/main/java/com/qiaoyn/study/Test4.java
@@ -0,0 +1,12 @@
+package com.qiaoyn.study;
+
+/**
+ * @author qiaoyn
+ * @description
+ * @date 2020/10/9
+ */
+public class Test4 {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
